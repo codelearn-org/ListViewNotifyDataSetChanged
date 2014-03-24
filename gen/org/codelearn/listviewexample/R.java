55c5cfceae365e2b31df687c21641e42960c0275
pocha <pocha.sharma@gmail.com>
2014-03-24 21:02:34 +0530

    notifyDataSetChanged first commit

--- gen/org/codelearn/listviewexample/R.java
+++ gen/org/codelearn/listviewexample/R.java
@@ -25,15 +25,11 @@ public final class R {
         public static final int user_profile=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int imageView1=0x7f080000;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f080003;
+        public static final int action_settings=0x7f080001;
+        public static final int listView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int tweet_row=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/org/codelearn/listviewexample/MainActivity.java
+++ src/org/codelearn/listviewexample/MainActivity.java
@@ -1,8 +1,12 @@
 package org.codelearn.listviewexample;
 
+import java.util.ArrayList;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,26 +15,63 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-    	ArrayAdapter adapter = new CustomAdapter(this);
-		setListAdapter(adapter);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		final ArrayList<String> list = new ArrayList<String>();
+    	final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+    	
+		ListView listView = (ListView) findViewById(R.id.listView1);
+		listView.setAdapter(adapter);
+		
+	    new UpdateAdapter(adapter,list).execute();
 	}
 	
-	@Override
-	protected void onListItemClick(ListView parent, View listItem, int position, long id) {
-		TextView title = (TextView) listItem.findViewById(R.id.textView1);
-		title.setText("item clicked");
+	private class UpdateAdapter extends AsyncTask<Void, Void, Void> {
+		
+		private ArrayAdapter adapter;
+		private ArrayList<String> list;
+		
+		public UpdateAdapter(ArrayAdapter adapterRef, ArrayList<String> listRef){
+			adapter = adapterRef;
+			list = listRef;
+		}
+
+		@Override
+		protected Void doInBackground(Void...voids) {	
+			for (int i = 0; i < 10; ++i) {			
+				 try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			    list.add("List item " + i);
+			    publishProgress();
+			    Log.d("codelearn","thread ran");
+			}
+			for (int i = 0; i < 10; ++i) {			
+				 try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			    list.remove(9 - i);
+			    publishProgress();
+			    Log.d("codelearn","thread ran");
+			}
+			return null;
+		}
+		
+		
+		protected void onProgressUpdate(Void...voids){
+			adapter.notifyDataSetChanged();
+		}
+		
 	}
 }
